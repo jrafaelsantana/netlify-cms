9496e239e82d8a8fb41f1b2806905feccb99f119
Shawn Erquhart <shawn@erquh.art>
2017-11-02 17:34:06 -0400

    fix media library pending state UX

--- src/components/MediaLibrary/MediaLibraryFooter.js
+++ src/components/MediaLibrary/MediaLibraryFooter.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, BrowseButton } from 'react-toolbox/lib/button';
+import Loader from '../UI/loader/Loader';
 
 const MediaLibraryFooter = ({
   onDelete,
@@ -9,40 +10,55 @@ const MediaLibraryFooter = ({
   hasSelection,
   forImage,
   canInsert,
-}) =>
-  <div>
-    <Button
-      label="Delete"
-      onClick={onDelete}
-      className="nc-mediaLibrary-buttonLeft"
-      disabled={!hasSelection}
-      accent
-      raised
-    />
-    <BrowseButton
-      label="Upload"
-      accept={forImage}
-      onChange={onPersist}
-      className="nc-mediaLibrary-buttonLeft"
-      primary
-      raised
-    />
-    <Button
-      label="Close"
-      onClick={onClose}
-      className="nc-mediaLibrary-buttonRight"
-      raised
-    />
-    { !canInsert ? null :
+  isPersisting,
+  isDeleting,
+}) => {
+  const shouldShowLoader = isPersisting || isDeleting;
+  const loaderText = isPersisting ? 'Uploading...' : 'Deleting...';
+  const loader = (
+    <div className="nc-mediaLibrary-button-loader">
+      <Loader className="nc-mediaLibrary-button-loaderSpinner" active/>
+      <strong className="nc-mediaLibrary-button-loaderText">{loaderText}</strong>
+    </div>
+  );
+  return (
+    <div>
       <Button
-        label="Insert"
-        onClick={onInsert}
-        className="nc-mediaLibrary-buttonRight"
-        disabled={!hasSelection}
+        label="Delete"
+        onClick={onDelete}
+        className="nc-mediaLibrary-buttonLeft"
+        disabled={shouldShowLoader || !hasSelection}
+        accent
+        raised
+      />
+      <BrowseButton
+        label="Upload"
+        accept={forImage}
+        onChange={onPersist}
+        className="nc-mediaLibrary-buttonLeft"
+        disabled={shouldShowLoader}
         primary
         raised
       />
-    }
-  </div>;
+      { shouldShowLoader ? loader : null }
+      <Button
+        label="Close"
+        onClick={onClose}
+        className="nc-mediaLibrary-buttonRight"
+        raised
+      />
+      { !canInsert ? null :
+        <Button
+          label="Insert"
+          onClick={onInsert}
+          className="nc-mediaLibrary-buttonRight"
+          disabled={!hasSelection}
+          primary
+          raised
+        />
+      }
+    </div>
+  );
+};
 
 export default MediaLibraryFooter;
